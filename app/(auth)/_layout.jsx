99ca8c16fca80a4c6e34a9d60fb0141ecc857879
Myint Myat Pyae Sone <myintmyatps664@gmail.com>
2024-10-24 20:47:24 +0630

    Complete Authentication feature

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -8,6 +8,14 @@ const AuthLayout = () => {
     <Stack screenOptions={{ headerShown: false }}>
       {/* The Stack will automatically pick up the screens from the (auth) folder */}
     </Stack>
+  //   <>
+  //   <Stack>
+  //     <Stack.screen name="sign-in" options={{ headerShown: false }} />
+  //     <Stack.screen name="sign-up" options={{ headerShown: false }} />
+  //   </Stack>
+
+  //   <StatusBar backgroundColor="#3BFB06" style="light" /> 
+  // </>
   );
 };
 
